20d1b5e24eec479307eb3b71606c595ff9bff96e
Fabrizio Demaria <fabriziodemaria@fabrymac.station>
2013-10-04 18:51:39 +0200

    Initial Commit

---
+++ FlipCardsAlpha/FCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FCAppDelegate.h
+//  FlipCardsAlpha
+//
+//  Created by Fabrizio Demaria on 04/10/13.
+//  Copyright (c) 2013 Fabrizio Demaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FlipCardsAlpha/FCViewController.h
@@ -0,0 +1,13 @@
+//
+//  FCViewController.h
+//  FlipCardsAlpha
+//
+//  Created by Fabrizio Demaria on 04/10/13.
+//  Copyright (c) 2013 Fabrizio Demaria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FCViewController : UIViewController
+
+@end
